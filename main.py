9ff720c242ba6f9602774399835bef934769f5c1
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-08-08 22:50:49 -0700

    Loop through the job list and apply to each job listing.

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ PASSWORD = "ENTER PASSWORD"
 chrome_driver_path = "ENTER CHROME DRIVER PATH"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=marketing%20intern&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+#UPDATE THE LINK TO YOUR OWN PREFERENCE
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=106233382&keywords=software%20engineer&location=San%20Jose%2C%20California%2C%20United%20States")
 
 #SIGN IN
 time.sleep(5)
@@ -24,3 +25,14 @@ password.send_keys(f'{PASSWORD}')
 
 sign_in_button = driver.find_element_by_class_name('login__form_action_container ')
 sign_in_button.click()
+
+
+job_list = driver.find_elements_by_css_selector(".job-card-container--clickable")
+for job in job_list:
+    job.click()
+    time.sleep(2)
+
+    save_button = driver.find_element_by_css_selector(".jobs-save-button")
+    save_button.click()
+
+    time.sleep(5)
